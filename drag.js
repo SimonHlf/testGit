3f2c0a50c4618782344845eb29bd1d2481d88eb1
simonHlf <simonhlf@163.com>
2018-03-21 14:18:45 +0800

    modify drag.js from master

--- drag.js
+++ drag.js
@@ -6,5 +6,9 @@ function drag(id){
 	obj.onmousedown = function(ev){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
+		document.onmousedown = function(ev){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		}
 	}
 }
